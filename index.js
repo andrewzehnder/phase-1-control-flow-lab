2ba4fea23b21b04cca570e789b4e11d6d568cd7d
Andrew <azehnder2@gmail.com>
2022-04-05 19:34:37 -0700

    commit tree 004616897f8a916582fe7f2d98df2530eaf8693d

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400){
+      result = 'This one is on me!'
+  } else if (feet > 400 && feet < 2000) {
+      result = 'That will be twenty bucks.'
+  } else if (feet > 2000 && feet < 2500) {
+      result = 'I will gladly take your thirty bucks.'
+  } else {
+      result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(desiredCity){
+  let result;
+  desiredCity === "NYC" ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipamount){
+  let result
+  switch (tipamount) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.'
+      break;
+  } 
+  return result
 }
